d9592f0e887859f748c392f68feff79e086826b9
Skuli <skuli@mbp.local>
2010-04-09 13:12:43 +0000

    Edited the program input file, so a missing file will not cause a crash.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# Compilers - Project 2
+# Compilers - Project 3
 # Reykjavik University, Spring 2010
 # Haukur Jonasson, Skuli Arnlaugsson
 
-# This is the lexical analyser file as requested in part 5 of the assignment.
-# -*- coding: utf-8 -*-
 import os
 import sys
 from ply.lex import lex
@@ -12,6 +10,9 @@ from parse import compParser
 def sep():
     print '--------------------------------------------------------------'
 
+def checkIfExists(filename):
+    import os
+    return os.path.exists(filename)
 
 def main(*args):
     usage = """
@@ -36,15 +37,11 @@ def main(*args):
       -  pas_syntax_ok     
       -  pas_syntax_err
       -  functionCall
-
-Using default input file."""
-    # TODO: Add options for the TAC code (print to screen or file)
-    # Generate our parser, with the given input
+"""
 
     tree, tac, table, writeToFile = False, False, False, False
-    inputFile = 'example'
+    inputFile = 'pas_code_if'
     outputFile = 'default.tac'
-
 
     if len(sys.argv) > 1:
         if sys.argv[1].lower() not in ('tree','tac','table'):
@@ -56,72 +53,61 @@ Using default input file."""
                 else:
                     outputFile = param + '.tac'
                     writeToFile = True
-        """
-        if sys.argv[1].lower() not in ('tree','tac','table'):
-            inputFile = sys.argv[1]
-            for param in sys.argv[2:]:
-                if param.lower()    == 'trace'     : tree  = True
-                elif param.lower()  == 'tac'     : tac   = True
-                elif param.lower()  == 'table'   : table = True
-                else:
-                    outputFile = param + '.tac'
-                    writeToFile = True
-        else:
-            for param in sys.argv[1:]:
-                if param.lower()    == 'trace'     : tree  = True
-                elif param.lower()  == 'tac'     : tac   = True
-                elif param.lower()  == 'table'   : table = True
-                else:
-                    outputFile = param + '.tac'
-                    writeToFile = True
-        """
     else:
         print usage
+        print 'Using the default input file.'
 
     filename = 'input'+os.sep+inputFile
     outputFile = 'output'+os.sep+outputFile
     sep()
     print 'Using input "%s"'%filename
-    parser = compParser(filename)
 
-    # Start parsing!
-    parser.parse(tree)
+    if checkIfExists(filename):
+        parser = compParser(filename)
 
-    fileHandler = open(filename)
-    pointer = 0
 
-    if len(parser.errors) > 0:
-        sep()
+        # Start parsing!
+        parser.parse(tree)
 
-    if not tree:
-        while True:
-            pointer += 1                                        
-            line = fileHandler.readline()
-            if not line: break
-            lineErrors = []
-            for error in parser.errors:
-                if error.lineno == pointer:
-                    lineErrors.append(error)
+        fileHandler = open(filename)
+        pointer = 0
 
-            if len(lineErrors) > 0:
-                if pointer < 10: print ' %d:\t%s'%(pointer,line),
-                else: print '%d:\t%s'%(pointer,line),
-                for error in lineErrors:
-                    print error.pointPrint()
+        if len(parser.errors) > 0:
+            sep()
 
-    if tree: sep()
+        if not tree:
+            while True:
+                pointer += 1
+                line = fileHandler.readline()
+                if not line: break
+                lineErrors = []
+                for error in parser.errors:
+                    if error.lineno == pointer:
+                        lineErrors.append(error)
 
-    if len(parser.errors) == 0:
-        print 'No errors were detected.'
-        sep()
-        if table:   parser.SymbolTable.__repr__()
-        if tac:     parser.printCode()
-        if writeToFile:
-            parser.printTacToFile(outputFile)
+                if len(lineErrors) > 0:
+                    if pointer < 10: print ' %d:\t%s'%(pointer,line),
+                    else: print '%d:\t%s'%(pointer,line),
+                    for error in lineErrors:
+                        print error.pointPrint()
+
+        if tree: sep()
+
+        if len(parser.errors) == 0:
+            print 'No errors were detected.'
+            sep()
+            if table:   parser.SymbolTable.__repr__()
+            if tac:     parser.printCode()
+            if writeToFile:
+                parser.printTacToFile(outputFile)
+        else:
+            sep()
+            print '%d errors were encountered.'%len(parser.errors)
+            sep()
+
     else:
-        sep()
-        print '%d errors were encountered.'%len(parser.errors)
-        sep()
+        print 'File: %s was not found.'%filename
+        print usage
 
 if __name__ == '__main__':
 	sys.exit(main(*sys.argv))
